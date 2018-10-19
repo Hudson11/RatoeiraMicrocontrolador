4ee391808149139c7eac7c347a37408c849156c8
Hudson Silva de Borba <hudsonhd36@gmail.com>
2018-10-18 21:49:02 -0300

    versão otimizada

--- RatoeiraMicrocontrolador.ino
+++ RatoeiraMicrocontrolador.ino
@@ -2,11 +2,12 @@
   DECLARANDO AS CONSTANTES DOS PINOS DE SAIDAS DIGITAIS E ENTRADAS ANALÓGICAS
 */
 
-#define buzzer  7
+#define buzzer  6
 #define motor  5
 #define pot  A0
 #define ldr  A1
-#define led  3
+#define led  4
+#define rele 3
 
 int valuePot = 0;
 int valueLdr = 0;
@@ -26,6 +27,7 @@ void setup()
   pinMode(motor, OUTPUT);
   pinMode(ldr, INPUT);
   pinMode(pot, INPUT);
+  pinMode(rele, OUTPUT);
   
   Serial.begin(9600);
   Serial.println("Teste Projeto Ratoeira");
@@ -34,6 +36,7 @@ void setup()
 void loop()
 {
 
+  digitalWrite(led, HIGH);
   /*
     MAPEAMENTO DA LEITURA ANALÓGICA DO LDR E POTENCIOMETRO
   */
@@ -46,17 +49,17 @@ void loop()
   Serial.print("Valor Lido pelo Potenciometro: ");
   Serial.println(valuePot);
 
-  
   /*
     VALUEPOT É USADA PARA AJUSTAR O CONDICIONAL DADO A ILUINAÇÃO DO AMBIENTE
       ONDE É REALIZADO TESTE
   */
   if(test == 0){
     if(valueLdr > valuePot){
-      analogWrite(motor, speedMotor);
-      delay(200);
-      digitalWrite(buzzer, HIGH);
-      digitalWrite(motor, LOW);
+      analogWrite(rele, HIGH);
+      delay(400);
+      digitalWrite(rele, LOW);
+      tone(buzzer, 440);
+      delay(3000);
       /*
           TEST INICIALMENTE 0, SERÁ ATIVADA APÓS O RATO ENTRAR NA RATOEIRA E SEJA CAPTURADO,
         BASICAMENTE NOS IMPEDIMOS QUE ELE ENCOSTE NOVAMENTE NO LDR, ASSIM, ACIONANDO NOVAMENTE
@@ -65,8 +68,8 @@ void loop()
       test = 1;
     }
   }else{
-    digitalWrite(motor, LOW);
-    digitalWrite(buzzer, LOW);
+    digitalWrite(rele, LOW);
+    noTone(buzzer);
   }
   
 }
